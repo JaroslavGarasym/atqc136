1cbafdd1fd976aa8e32a5eb12882433728448cf5
JaroslavGarasym <garasym@gmail.com>
2015-01-22 02:26:51 +0200

    first

---
+++ src/test/java/com/softserve/atqc/AppTest.java
@@ -0,0 +1,20 @@
+package com.softserve.atqc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class AppTest {
+
+	@Test
+	public void testApp() {
+		System.out.println("Done testApp()");
+		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void testApp2() {
+		System.out.println("Done testApp2()");
+		Assert.assertTrue(true);
+	}
+}
