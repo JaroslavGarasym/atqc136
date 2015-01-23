bdd8ed56f93bbccde77111553e1abd48238acff2
JaroslavGarasym <garasym@gmail.com>
2015-01-23 15:52:28 +0200

    Comment

--- src/test/java/com/softserve/atqc/AppTest.java
+++ src/test/java/com/softserve/atqc/AppTest.java
@@ -8,13 +8,13 @@ public class AppTest {
 
 	@Test
 	public void testApp() {
-		System.out.println("Done testApp()");
+		//System.out.println("Done testApp()");
 		Assert.assertTrue(true);
 	}
 
 	@Test
 	public void testApp2() {
-		System.out.println("Done testApp2()");
+		//System.out.println("Done testApp2()");
 		Assert.assertTrue(true);
 	}
 }
